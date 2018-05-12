ba45866d4ef6e121b4f292f9674aa9be66ccc687
pando <ndjman7@gmail.com>
2018-05-12 12:03:36 +0900

    Add static & form & user

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', user_views.signup, name='signup'),
 ]
